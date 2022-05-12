a3a29398e185b3c6fd363532926f31406e2cc33d
AtanasKisyov <atanaskisyov9@gmail.com>
2022-05-12 22:01:51 +0300

    add method to API class

--- bundesliga_app/bundesliga_api/helpers/OpenLigaAPI.py
+++ bundesliga_app/bundesliga_api/helpers/OpenLigaAPI.py
@@ -7,12 +7,14 @@ class API:
     HOST = 'https://api.openligadb.de/'
     MATCH_DATA_SUFFIX = 'getmatchdata/'
     TABLE_SUFFIX = 'getbltable/'
+    TEAMS_SUFFIX = 'getavailableteams/'
+    MATCHES_BY_TEAM_SUFFIX = 'getmatchesbyteamid/'
     BUNDESLIGA_ID = 'bl1/'
     SEASON = '2021/'
     URL = HOST + BUNDESLIGA_ID + SEASON
 
-    def get_all_matches(self):
-        url = self.HOST + self.MATCH_DATA_SUFFIX + self.BUNDESLIGA_ID + self.SEASON
+    def get_all_matches(self, team=''):
+        url = self.HOST + self.MATCH_DATA_SUFFIX + self.BUNDESLIGA_ID + self.SEASON + team
         matches = requests.get(url)
         return json.loads(matches.content)
 
@@ -25,3 +27,15 @@ class API:
         url = self.HOST + self.TABLE_SUFFIX + self.BUNDESLIGA_ID + self.SEASON
         table = requests.get(url)
         return json.loads(table.content)
+
+    def get_team_names(self):
+        url = self.HOST + self.TEAMS_SUFFIX + self.BUNDESLIGA_ID + self.SEASON
+        response = requests.get(url)
+        return json.loads(response.content)
+
+    def get_team_last_five_matches_and_next_match(self, team_id):
+        last_five_parameter = '/5'
+        next_match_parameter = '/1'
+        url = self.HOST + self.MATCHES_BY_TEAM_SUFFIX + str(team_id) + last_five_parameter + next_match_parameter
+        response = requests.get(url)
+        return json.loads(response.content)
