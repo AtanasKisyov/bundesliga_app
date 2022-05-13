a6d53bec26df985e18080c57423cb7aaca9b2d98
AtanasKisyov <atanaskisyov9@gmail.com>
2022-05-13 22:39:50 +0300

    update urls

--- bundesliga_app/bundesliga_api/urls.py
+++ bundesliga_app/bundesliga_api/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from bundesliga_app.bundesliga_api.views import home_view, all_matchdays_view, select_matchday_view, \
+from bundesliga_app.bundesliga_api.views import home_view, select_matchday_view, \
     last_matchday_view, next_matchday_view, search_by_team
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('matchdays/', all_matchdays_view, name='all_matchdays'),
     path('matchday/<int:pk>', select_matchday_view, name='select_matchday'),
     path('last-matchday/', last_matchday_view, name='last_matchday'),
     path('next-matchday/', next_matchday_view, name='next_matchday'),
